5570ba8b2cf875a8c4fe5446025b6b41fc93ee8b
rachid <azeroual.rachid1@gmail.com>
2022-06-07 12:27:39 -0400

    addded groupbydepart

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Employee {
 
@@ -80,18 +82,40 @@ public class Employee {
         this.salary = salary;
     }
 
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", department='" + department + '\'' +
+                ", yearOfJoining=" + yearOfJoining +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public static Map<String, Long> countEmployeeByDepartment(List<Employee> listRec){
+
+        return listRec.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
+    }
+
     public static void main(String[] args) {
 
         List<Employee> employees = new ArrayList<Employee>();
-        new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0);
-        new Employee(122, "Paul Niksui", 25, "Male", "Sales And Marketing", 2015, 13500.0);
-        new Employee(133, "Martin Theron", 29, "Male", "Infrastructure", 2012, 18000.0);
-        new Employee(144, "Murali Gowda", 28, "Male", "Product Development", 2014, 32500.0);
-        new Employee(155, "Nima Roy", 27, "Female", "HR", 2013, 22700.0);
-        new Employee(211, "Jasna Kaur", 27, "Female", "Infrastructure", 2014, 15700.0);
-        new Employee(166, "Iqbal Hussain", 43, "Male", "Security And Transport", 2016, 10500.0);
-        new Employee(266, "Sanvi Pandey", 26, "Female", "Product Development", 2015, 28900.0);
-        new Employee(277, "Anuj", 31, "Male", "Product Development", 2012, 35700.0);
+        employees.add(new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0));
+        employees.add(new Employee(122, "Paul Niksui", 25, "Male", "Sales And Marketing", 2015, 13500.0));
+        employees.add(new Employee(133, "Martin Theron", 29, "Male", "Infrastructure", 2012, 18000.0));
+        employees.add(new Employee(144, "Murali Gowda", 28, "Male", "Product Development", 2014, 32500.0));
+        employees.add(new Employee(155, "Nima Roy", 27, "Female", "HR", 2013, 22700.0));
+        employees.add(new Employee(211, "Jasna Kaur", 27, "Female", "Infrastructure", 2014, 15700.0));
+        employees.add(new Employee(166, "Iqbal Hussain", 43, "Male", "Security And Transport", 2016, 10500.0));
+        employees.add(new Employee(266, "Sanvi Pandey", 26, "Female", "Product Development", 2015, 28900.0));
+        employees.add(new Employee(277, "Anuj", 31, "Male", "Product Development", 2012, 35700.0));
+
+        //printing
+        System.out.println(countEmployeeByDepartment(employees));
+        
     }
 
 
